b7ef2843fd55e44ddabf002bd219b2846f2b3520
puroh <diego683@gmail.com>
2015-08-25 10:07:52 -0500

    Se estandarizo el texto

---
+++ registros/alu.h
@@ -0,0 +1,6 @@
+unsigned long int ADD(unsigned long int R0, unsigned long int R1);
+unsigned long int SUB(unsigned long int R0,unsigned long int R1);
+unsigned long int AND(unsigned long int R0, unsigned long int R1);
+unsigned long int ORR(unsigned long int R0,unsigned long int R1);
+unsigned long int EOR(unsigned long int R0,unsigned long int R1);
+unsigned long int MOV(unsigned long int R0,unsigned long int R1);
