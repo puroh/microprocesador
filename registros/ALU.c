764a9b1b33f670e362dc95c57d33ed4bb9307e7f
puroh <diego683@gmail.com>
2015-08-25 10:01:29 -0500

    Se corrigieron algunos detalles en los nombres de lass funciones

--- registros/ALU.c
+++ registros/ALU.c
@@ -1,9 +1,9 @@
-unsigned long int suma(unsigned long int R0,unsigned long int R1)
+unsigned long int ADD(unsigned long int R0,unsigned long int R1)
 {
     return R0+R1;
 
 }
-unsigned long int resta(unsigned long int R0,unsigned long int R1)
+unsigned long int SUB(unsigned long int R0,unsigned long int R1)
 {
     return R0-R1;
 
--- registros/ALU.h
+++ registros/ALU.h
@@ -1,5 +1,5 @@
-unsigned long int suma(unsigned long int R0, unsigned long int R1);
-unsigned long int resta(unsigned long int R0,unsigned long int R1);
+unsigned long int ADD(unsigned long int R0, unsigned long int R1);
+unsigned long int SUB(unsigned long int R0,unsigned long int R1);
 unsigned long int ANDS(unsigned long int R0, unsigned long int R1);
 unsigned long int ORES(unsigned long int R0,unsigned long int R1);
 unsigned long int XOR(unsigned long int R0,unsigned long int R1);
