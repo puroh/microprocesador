0f5fef31266c1b2c7898f2e38752fea0a1b5df00
bavv326 <villalex326@outlook.com>
2015-08-28 11:24:21 -0500

    Se comentan los archivos alu.h y idd.h debido a modificaciones en el código

--- registros/idd.h
+++ registros/idd.h
@@ -1,7 +1,31 @@
 #include <stdint.h>
+/**
+*	\brief Función de desplazamiento lógico a la izquierda
+*	\param Rm Operador con signo de 32 bits
+*	\param Rn Operador con signo de 32 bits
+*	\return Rd Un entero con signo de 32 bits que contiene el resultado
+*/ 
 uint32_t LSLS(uint32_t Rm,uint32_t Rn,uint32_t Rd);
+/**
+*	\brief Función de desplazamiento lógico a la derecha 
+*	\param Rm Operador con signo de 32 bits
+*	\param Rn Operador con signo de 32 bits
+*	\return Rd Un entero con signo de 32 bits que contiene el resultado
+*/ 
 uint32_t LSRS(uint32_t Rm,uint32_t Rn,uint32_t Rd);
+/**
+*	\brief Función de rotación a la derecha
+*	\param Rm Operador con signo de 32 bits
+*	\param Rn Operador con signo de 32 bits
+*	\return Rd Un entero con signo de 32 bits que contiene el resultado
+*/ 
 uint32_t ROR(uint32_t Rm,uint32_t Rn,uint32_t Rd);
+/**
+*	\brief Función de desplazamiento aritmético lógico
+*	\param Rm Operador con signo de 32 bits
+*	\param Rn Operador con signo de 32 bits
+*	\return Rd Un entero con signo de 32 bits que contiene el resultado
+*/ 
 uint32_t ASRS(uint32_t Rm,uint32_t Rn,uint32_t Rd);
 
 
