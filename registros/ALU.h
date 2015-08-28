50a67b36c83ed2b4e0a91034c13fc84fef4a27b2
puroh <diego683@gmail.com>
2015-08-27 23:06:02 -0500

    se borraron archivos que el .ignore no ignoro, como tambien archivos que ya no se usan

--- registros/ALU.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-uint32_t ADD(uint32_t Rm,uint32_t Rn,uint32_t Rd);
-uint32_t SUB(uint32_t Rm,uint32_t Rn,uint32_t Rd);
-uint32_t AND(uint32_t Rm,uint32_t Rn,uint32_t Rd);
-uint32_t ORR(uint32_t Rm,uint32_t Rn,uint32_t Rd);
-uint32_t EOR(uint32_t Rm,uint32_t Rn,uint32_t Rd);
-uint32_t MOV(uint32_t Rm,uint32_t Rn,uint32_t Rd);
