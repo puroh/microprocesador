2a9eb8d7545ea1c2d4d0d05ada2fc56fa933350d
bavv326 <villalex326@outlook.com>
2015-08-28 14:48:56 -0500

    Se corrigen los .c (alu, idd, registros en donde se organizan los códigos y se tabulan apropiadamente)

--- registros/registros.c
+++ registros/registros.c
@@ -1,8 +1,8 @@
 void registros1(long int *registros)
 {
- int i;
-    for(i=0;i<4;i++){
-    printf("%d %d %d\n",registros[i],registros[i+1],registros[i+2]);
-                    }
-
+	int i;
+    for(i=0;i<4;i++)
+	{
+		printf("%d %d %d\n",registros[i],registros[i+1],registros[i+2]);
+    }
 }
