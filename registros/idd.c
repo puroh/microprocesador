e81a6da99d0ad37fe1933d1ff6bf4d3ae74f455d
mauricio060695 <mauro.cas95@hotmail.com>
2015-08-28 21:31:01 -0500

    se añade la funcion REV y REV16 junto con las variaciones de la alu en punteros

--- registros/idd.c
+++ registros/idd.c
@@ -14,3 +14,14 @@ void ASRS(uint32_t *Rm,uint32_t *Rn){
    *Rn=~0<<(32-*Rn);
    *Rm=(*Rm>>1)|*Rn;
 }
+void REV(uint32_t *Rm,uint32_t *Rn){
+     *Rn=*Rm<<24;
+     *Rn=*Rn|*Rm>>24;
+     *Rm=*Rn|((((*Rm<<8)>>24)<<8)|(((*Rm<<16)>>24)<<16));
+}
+void REV16(uint32_t *Rm,uint32_t *Rn){
+     *Rn=((*Rm<<24)>>16)|((*Rm<<16)>>24);
+     *Rm=*Rn|(((Rm>>24)<<16)|((Rm>>16)<<24));
+}
+
+
--- registros/idd.h
+++ registros/idd.h
@@ -3,5 +3,7 @@ void LSLS(uint32_t *Rm,uint32_t *Rn);
 void LSRS(uint32_t *Rm,uint32_t *Rn);
 void ROR(uint32_t *Rm,uint32_t *Rn);
 void ASRS(uint32_t *Rm,uint32_t *Rn);
+void REV(uint32_t *Rm,uint32_t *Rn);
+void REV16(uint32_t *Rm,uint32_t *Rn);
 
 
