b0a0a5fbf2fc408c6d2fcdc6cff6cd56aa56f609
puroh <diego683@gmail.com>
2015-08-28 22:39:50 -0500

    se agregaron punteros y registro de desplazamiento

--- registros/alu.c
+++ registros/alu.c
@@ -1,21 +1,4 @@
 #include <stdint.h>
-<<<<<<< HEAD:registros/alu.c
-uint32_t ADD(uint32_t Rm,uint32_t Rn,uint32_t Rd)
-{
-	uint8_t C=0,Z=0,N=0,V=0;
-    Rd=Rm+Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-		C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-        Z=1;
-    }
-    printf("\nZ:%d\n",Z);
-    return Rd;
-=======
 void ADD(uint32_t *Rm,uint32_t *Rn){
     uint8_t C=0,Z=0,N=0,V=0;
 
@@ -28,25 +11,9 @@ void ADD(uint32_t *Rm,uint32_t *Rn){
     }
     printf("\nZ:%d\n",Z);
 
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
 }
 void SUB(uint32_t *Rm,uint32_t *Rn)
 {
-<<<<<<< HEAD:registros/alu.c
-    uint8_t C=0,Z=0,N=0,V=0;
-    Rd=Rm-Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-        C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-        Z=1;
-    }
-    printf("\nZ:%d\n",Z);
-    return Rd;
-=======
      uint8_t C=0,Z=0,N=0,V=0;
 
     *Rm=*Rm-(~*Rn+1);
@@ -58,25 +25,11 @@ void SUB(uint32_t *Rm,uint32_t *Rn)
     }
     printf("\nZ:%d\n",Z);
 
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
+
 }
 void AND(uint32_t *Rm,uint32_t *Rn)
 {
-<<<<<<< HEAD:registros/alu.c
-    uint8_t C=0,Z=0,N=0,V=0;
-    Rd=Rm&Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-        C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-        Z=1;
-    }
-    printf("\nZ:%d\n",Z);
-    return Rd;
-=======
+
      uint8_t C=0,Z=0,N=0,V=0;
 
     *Rm=*Rm&*Rn;
@@ -88,25 +41,10 @@ void AND(uint32_t *Rm,uint32_t *Rn)
     }
     printf("\nZ:%d\n",Z);
 
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
 }
 void ORR(uint32_t *Rm,uint32_t *Rn)
 {
-<<<<<<< HEAD:registros/alu.c
-    uint8_t C=0,Z=0,N=0,V=0;
-    Rd=Rm|Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-        C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-        Z=1;
-    }
-    printf("\nZ:%d\n",Z);
-    return Rd;
-=======
+
      uint8_t C=0,Z=0,N=0,V=0;
 
     *Rm=*Rm|*Rn;
@@ -118,25 +56,10 @@ void ORR(uint32_t *Rm,uint32_t *Rn)
     }
     printf("\nZ:%d\n",Z);
 
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
+
 }
 void EOR(uint32_t *Rm,uint32_t *Rn)
 {
-<<<<<<< HEAD:registros/alu.c
-    uint8_t C=0,Z=0,N=0,V=0;
-    Rd=Rm^Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-        C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-        Z=1;
-    }
-    printf("\nZ:%d\n",Z);
-    return Rd;
-=======
      uint8_t C=0,Z=0,N=0,V=0;
 
     *Rm=*Rm^*Rn;
@@ -148,27 +71,16 @@ void EOR(uint32_t *Rm,uint32_t *Rn)
     }
     printf("\nZ:%d\n",Z);
 
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
 }
 void MOV(uint32_t *Rm,uint32_t *Rn)
 {
     uint8_t C=0,Z=0,N=0,V=0;
-<<<<<<< HEAD:registros/alu.c
-    Rd=Rm<<Rn;
-    if(Rm>=2147483648&&Rn<2147483648&&Rd<2147483648)
-	{
-        C=1;
-	}
-    printf("\nC:%d\n",C);
-    if(!Rd)
-	{
-=======
     Rn=Rm;//Rm lo manda a Rn
     if(*Rm<2147483648&&(*Rn<2147483648||*Rn>=2147483648)){
         C=1;}
     printf("\nC:%d\n",C);
     if(!*Rm){
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51:registros/ALU.c
+
         Z=1;
     }
     printf("\nZ:%d\n",Z);
--- registros/idd.c
+++ registros/idd.c
@@ -1,22 +1,5 @@
 #include <stdint.h>
-<<<<<<< HEAD
-uint32_t LSLS(uint32_t Rm,uint32_t Rn,uint32_t Rd)
-{
-	Rd=Rm<<Rn;
-	return Rd;
-}
-uint32_t LSRS(uint32_t Rm,uint32_t Rn,uint32_t Rd)
-{
-	Rd=Rm>>Rn;
-	return Rd;
-}
-uint32_t ROR(uint32_t Rm,uint32_t Rn,uint32_t Rd)
-{
-	Rd=Rm<<(32-Rn);
-	Rm=Rm>>Rn;
-	Rd=Rm|Rd;
-    return Rd;
-}
+
 uint32_t BIC(uint32_t Rm,uint32_t Rn,uint32_t Rd)
 {
 	Rd &= ~Rm;
@@ -33,7 +16,7 @@ uint32_t MVN(uint32_t Rm,uint32_t Rn,uint32_t Rd)
 }
 void NOP()
 {
-=======
+
 void LSLS(uint32_t *Rm,uint32_t *Rn){
   *Rm=*Rm<<*Rn;
 }
@@ -48,10 +31,14 @@ void ROR(uint32_t *Rm,uint32_t *Rn){
 void ASRS(uint32_t *Rm,uint32_t *Rn){
    *Rn=~0<<(32-*Rn);
    *Rm=(*Rm>>1)|*Rn;
->>>>>>> 617f899605fcc84aef36460062dea83e6bd8de51
 }
-uint32_t REV(uint32_t Rm,uint32_t Rn,uint32_t Rd)
-{
- 
+void REV(uint32_t *Rm,uint32_t *Rn){
+     *Rn=*Rm<<24;
+     *Rn=*Rn|*Rm>>24;
+     *Rm=*Rn|((((*Rm<<8)>>24)<<8)|(((*Rm<<16)>>24)<<16));
+}
+void REV16(uint32_t *Rm,uint32_t *Rn){
+     *Rn=((*Rm<<24)>>16)|((*Rm<<16)>>24);
+     *Rm=*Rn|(((Rm>>24)<<16)|((Rm>>16)<<24));
 }
 
