ffe61d484b47c93da2de95c3ccd2993dfb96170c
puroh <diego683@gmail.com>
2015-09-08 11:05:18 -0500

    Se cambia % por // en algunos comentarios

--- registros/flags.c
+++ registros/flags.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "alu.h"
-void flag(uint32_t *Rm) %Se crea una serie de condiciones para determinar las banderas de cualquier función 
+void flag(uint32_t *Rm) //Se crea una serie de condiciones para determinar las banderas de cualquier función
 {
 	uint32_t x;
 	x=1;
 	bool flags[3];
-	if(((*Rm<<32)>>32)&1==1) %Evalua para determinar el valor de la bandera N
+	if(((*Rm<<32)>>32)&1==1) //Evalua para determinar el valor de la bandera N
 	{
 		flags[N]=1;
 	}
@@ -14,7 +14,7 @@ void flag(uint32_t *Rm) %Se crea una serie de condiciones para determinar las ba
 	{
 		flags[N]=0;
 	}
-	if(!*Rm) %Evalua para determinar el valor de la bandera Z
+	if(!*Rm) //Evalua para determinar el valor de la bandera Z
 	{
 		flags[Z]=1;
 	}
@@ -22,7 +22,7 @@ void flag(uint32_t *Rm) %Se crea una serie de condiciones para determinar las ba
 	{
 		flags[Z]=0;
 	}
-	if(*Rm>(x<<32)) %Evalua para determinar el valor de la bandera C
+	if(*Rm>(x<<32)) //Evalua para determinar el valor de la bandera C
 	{
 		flags[C]=1;
 	}
@@ -31,5 +31,5 @@ void flag(uint32_t *Rm) %Se crea una serie de condiciones para determinar las ba
 		flags[C]=0;
 	}
 	move(6,15);
-	printf("signo %d zero %d acarreo %d\n",flags[N],flags[Z],flags[C]); %Imprime en pantalla el valor de las banderas según las condiciones establecidas anteriormente.
+	printf("signo %d zero %d acarreo %d\n",flags[N],flags[Z],flags[C]); //Imprime en pantalla el valor de las banderas según las condiciones establecidas anteriormente.
 }
