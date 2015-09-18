5e958d828f5b5191a619ad79355dbbfc4de6492f
puroh <diego683@gmail.com>
2015-09-18 12:37:58 -0500

    Se revisaron los cambios y se modificaron algunos problemas con el merge

--- registros/alu.c
+++ registros/alu.c
@@ -39,44 +39,8 @@ void EOR(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn)
     *Rd=*Rm^*Rn;
     comp=1;
     flag(Rd,Rm,Rn,banderas,&comp);
+}
 
-=======
-
-bool banderas[3];
-void obtenerBandera(bool *bands)
-{
-    bands=banderas;
-}
-void ADD(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn) /* Definen las direcciones que contiene los valores a operar y la dirección en donde se almacenara dicho resultado en este caso una suma */
-{
-    *Rd=*Rm+*Rn; /* Suma entre las direcciones que contiene  el valor de los registros a operar */
-	flag(Rd,Rm,Rn,banderas);
-}
-void SUB(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn) /* Definen las direcciones que contiene los valores a operar y la dirección en donde se almacenara dicho resultado en este caso una resta */
-{
-    *Rd=*Rm+((~*Rn)+1); 
-    flag(Rd,Rm,Rn,banderas);
-}
-void AND(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn) /* Definen las direcciones que contiene los valores a operar y la dirección en donde se almacenara dicho resultado en este caso una multiplicación */
-{
-    *Rd=*Rm&*Rn; /* Multiplicación entre las direcciones que contiene  el valor de los registros a operar */
-    flag(Rd,Rm,Rn,banderas);
-}
-void ORR(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn) 
-{
-    *Rd=*Rm|*Rn; 
-    flag(Rd,Rm,Rn,banderas);
->>>>>>> master
-}
-void EOR(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn) 
-{
-<<<<<<< HEAD
-
-    *Rm=*Rn;
-    comp=2;
-    flag(Rm,Rm,Rn,banderas,&comp);
-
-}
 void CMP(uint32_t *Rm,uint32_t *Rn)
 {
     uint32_t R;
@@ -105,7 +69,6 @@ void SBC(uint32_t *Rd,uint32_t *Rm,uint32_t *Rn)
 }
 void MOVS(uint32_t *Rm,uint32_t Rn)
 {
-
     *Rm=Rn;
     comp=2;
     flag(Rm,Rm,&Rn,banderas,&comp);
@@ -148,22 +111,21 @@ void ASRS(uint32_t *Rm,uint32_t *Rn)
    *Rm=(*Rm>>1)|*Rn;
    comp=1;
    flag(Rm,Rm,Rn,banderas,&comp);
-=======
-    *Rd=*Rm^*Rn; 
-    flag(Rd,Rm,Rn,banderas);
 }
 void MOV(uint32_t *Rm,uint32_t *Rn) /* Defienen las diracciones que contiene la dirección del valor a mover como también la dirección a donde se desea almacenar */
 {
     *Rm=*Rn; /* Igualación de las direcciones para copiar el valor de Rn en la dirección de Rm */
+	comp=2;
+	flag(Rm,Rm,Rn,banderas,&comp);
 }
 void CMP(uint32_t *Rm,uint32_t *Rn)
 {
     uint32_t R;
     R=*Rm+((~*Rn)+1);
+	comp=0;
     flag(&R,Rm,Rn,banderas);
 }
 
->>>>>>> master
 
 }
 void REV(uint32_t *Rm,uint32_t *Rn)
