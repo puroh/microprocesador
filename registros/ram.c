0a4a3c2cc7902577184a9a211cea396ccf005139
puroh <diego683@gmail.com>
2015-09-30 16:57:57 -0500

    se prepara para hacer un merget

--- registros/ram.c
+++ registros/ram.c
@@ -6,10 +6,17 @@
 #define MEMORIA 64
 #define DIRMAXMEM 255
 
-
+void inimemoria(uint32_t *memoria,int tama);
+void mostrar_memoria(uint32_t *memoria , int tama);
+void PUSH(uint32_t *registros,uint32_t *memory,uint8_t *res);
+void POP(uint32_t *registros,uint32_t *memory,uint8_t *res);
+int bitcount(int *R);
+void inicia_ram(void);
+void Disp(void);
 
 //WINDOW *newwin(35,35,0,0);
 //WINDOW *win;
+
 void inicia_ram(void)
 {
 	
