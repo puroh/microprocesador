bc02f8db5f184b87393ad51c2396595748fd3bf6
bavv326 <villalex326@outlook.com>
2015-10-02 23:08:02 -0500

    Se comentan y se tabulan algunos codigos

--- registros/ram.c
+++ registros/ram.c
@@ -1,80 +1,79 @@
 #include <stdint.h>
 #include "ram.h"
 #include <curses.h>
-uint8_t bitcount(uint8_t *R){
-
+uint8_t bitcount(uint8_t *R)
+{
 	uint8_t i,contador=0;
-	for(i=0;i<16;i++){
-	if((1 & R[i])!=0)
-       contador++;
-                    }
+	for(i=0;i<16;i++)
+	{
+		if((1 & R[i])!=0)
+		{
+			contador++;
+		}
+    }
     return contador;
 }
-void PUSH(uint32_t *registros,uint32_t *memory,uint8_t *res){
+void PUSH(uint32_t *registros,uint32_t *memory,uint8_t *res)
+{
     uint8_t address;
     uint8_t i=0;
     (registros[12])--;
     address=registros[12]-(4*bitcount(res));
-    for(i=0;i<15;i++){
-		if( (1  & res[i])!= 0 ){
-
+    for(i=0;i<15;i++)
+	{
+		if( (1  & res[i])!= 0 )
+		{
             memory[MEMORIA-((address/4)+2)]=registros[i];
-
-
 			address=address+4;
-			}
 		}
-	registros[12]-=(4*bitcount(res));
 	}
-
-void POP(uint32_t *registros,uint32_t *memory,uint8_t *res){
+	registros[12]-=(4*bitcount(res));
+}
+void POP(uint32_t *registros,uint32_t *memory,uint8_t *res)
+{
     uint8_t address;
     uint8_t i=0;
-
     address=registros[12];
-    for(i=0;i<7;i++){
-		if( (1 & res[i])!= 0 ){
-
-           registros[i]=memory[MEMORIA-((address/4)+2)];
-
-
+    for(i=0;i<7;i++)
+	{
+		if( (1 & res[i])!= 0 )
+		{
+			registros[i]=memory[MEMORIA-((address/4)+2)];
 			address=address+4;
-			}
 		}
+	}
 	registros[12]+=(4*bitcount(res));
 }
-
-
-
-
-
-void mostrar_memoria(uint32_t *memoria , int tama){
+void mostrar_memoria(uint32_t *memoria , int tama)
+{
     int i,j,h,k,l;
-        h=0;
-		l=0;
-		k=DIRMAXMEM;
-	    for(i=0;i<tama/8;i++)
-        {
-		for(j=0;j<tama/8;j++){
-		h+=15;
-		mvprintw(14+i,8+h,"%.2X : %.8X",k,memoria[l]);
-		l=l+1;
-		k=k-4;
-        refresh();
-
+    h=0;
+	l=0;
+	k=DIRMAXMEM;
+	for(i=0;i<tama/8;i++)
+    {
+		for(j=0;j<tama/8;j++)
+		{
+			h+=15;
+			mvprintw(14+i,8+h,"%.2X : %.8X",k,memoria[l]);
+			l=l+1;
+			k=k-4;
+			refresh();
         }
 		//tama = 64
 		h=0;
-		}
-
+	}
 }
-void inimemoria(uint32_t *memoria,int tama){
+void inimemoria(uint32_t *memoria,int tama)
+{
     int i;
     for(i=0;i<(tama*4);i++)
     {
         memoria[i]= -1;
     }
 }
+
+
 /*
 void inicia_ram(void)
 {
--- registros/ram.h
+++ registros/ram.h
@@ -1,9 +1,33 @@
 #include <stdint.h>
 #define MEMORIA 64
 #define DIRMAXMEM 255
-
+/**
+*	\brief Función de cambio de orden de los bytes en cada halfword de 16 bits
+*	\param Rm Dirección que contiene el operador con signo de 32 bits
+*	\param Rn Dirección que contiene el operador con signo de 32 bits
+*/
 void inimemoria(uint32_t *memoria,int tama);
+/**
+*	\brief Función de cambio de orden de los bytes en cada halfword de 16 bits
+*	\param Rm Dirección que contiene el operador con signo de 32 bits
+*	\param Rn Dirección que contiene el operador con signo de 32 bits
+*/
 void mostrar_memoria(uint32_t *memoria , int tama);
+/**
+*	\brief Función de cambio de orden de los bytes en cada halfword de 16 bits
+*	\param Rm Dirección que contiene el operador con signo de 32 bits
+*	\param Rn Dirección que contiene el operador con signo de 32 bits
+*/
 void PUSH(uint32_t *registros,uint32_t *memory,uint8_t *res);
+/**
+*	\brief Función de cambio de orden de los bytes en cada halfword de 16 bits
+*	\param Rm Dirección que contiene el operador con signo de 32 bits
+*	\param Rn Dirección que contiene el operador con signo de 32 bits
+*/
 void POP(uint32_t *registros,uint32_t *memory,uint8_t *res);
+/**
+*	\brief Función de cambio de orden de los bytes en cada halfword de 16 bits
+*	\param Rm Dirección que contiene el operador con signo de 32 bits
+*	\param Rn Dirección que contiene el operador con signo de 32 bits
+*/
 uint8_t bitcount(uint8_t *R);
